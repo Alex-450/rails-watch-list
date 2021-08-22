d6408f7a5f3a238955e333a7d7da375b68d6b50a
Alex Stearn <alex.wmstearn@gmail.com>
2021-08-22 11:08:57 +0200

    landing jumbotron changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     resources :bookmarks, only: [:create]
   end
   resources :bookmarks, only: :destroy
+  get :movies, to: 'movies#search'
   root to: 'lists#index'
 end
